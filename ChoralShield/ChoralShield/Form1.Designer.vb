33638568da0729edb6b775a0bb42421c302fca7f
onfiremaverick <da-dasboy@hotmail.com>
2017-02-16 20:44:29 +0000

    Add files via upload

---
+++ ChoralShield/ChoralShield/Form1.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btn_start = New System.Windows.Forms.Button()
+        Me.btn_clear = New System.Windows.Forms.Button()
+        Me.btn_exit = New System.Windows.Forms.Button()
+        Me.LbResults = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(74, 9)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(236, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Essell Academy Choral Shield "
+        '
+        'btn_start
+        '
+        Me.btn_start.Location = New System.Drawing.Point(12, 31)
+        Me.btn_start.Margin = New System.Windows.Forms.Padding(4)
+        Me.btn_start.Name = "btn_start"
+        Me.btn_start.Size = New System.Drawing.Size(112, 30)
+        Me.btn_start.TabIndex = 1
+        Me.btn_start.Text = "Start"
+        Me.btn_start.UseVisualStyleBackColor = True
+        '
+        'btn_clear
+        '
+        Me.btn_clear.Location = New System.Drawing.Point(132, 31)
+        Me.btn_clear.Margin = New System.Windows.Forms.Padding(4)
+        Me.btn_clear.Name = "btn_clear"
+        Me.btn_clear.Size = New System.Drawing.Size(112, 30)
+        Me.btn_clear.TabIndex = 2
+        Me.btn_clear.Text = "Clear"
+        Me.btn_clear.UseVisualStyleBackColor = True
+        '
+        'btn_exit
+        '
+        Me.btn_exit.Location = New System.Drawing.Point(252, 31)
+        Me.btn_exit.Margin = New System.Windows.Forms.Padding(4)
+        Me.btn_exit.Name = "btn_exit"
+        Me.btn_exit.Size = New System.Drawing.Size(112, 30)
+        Me.btn_exit.TabIndex = 2
+        Me.btn_exit.Text = "Exit"
+        Me.btn_exit.UseVisualStyleBackColor = True
+        '
+        'LbResults
+        '
+        Me.LbResults.FormattingEnabled = True
+        Me.LbResults.ItemHeight = 17
+        Me.LbResults.Location = New System.Drawing.Point(12, 68)
+        Me.LbResults.Name = "LbResults"
+        Me.LbResults.Size = New System.Drawing.Size(411, 157)
+        Me.LbResults.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(429, 230)
+        Me.Controls.Add(Me.LbResults)
+        Me.Controls.Add(Me.btn_exit)
+        Me.Controls.Add(Me.btn_clear)
+        Me.Controls.Add(Me.btn_start)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Lucida Fax", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btn_start As Button
+    Friend WithEvents btn_clear As Button
+    Friend WithEvents btn_exit As Button
+    Friend WithEvents LbResults As ListBox
+End Class
---
+++ ChoralShield/ChoralShield/Form1.vb
@@ -0,0 +1,97 @@
+﻿Public Class Form1
+    'Declaring Variables
+    Dim customerid(299) As String
+    Dim ticketid(299) As String
+    Dim numberoftickets(299) As Integer
+    Dim method(299) As String
+    'This Button is the main button
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_start.Click
+        'Sub Routines
+        'Sub Routine to read data From the document
+        Call readdata(customerid, ticketid, numberoftickets, method)
+        'Sub Routine To Calculate
+        Call calculations(customerid, ticketid, numberoftickets, method)
+        'Sub Routine To Write Data To Document
+        Call writedata(customerid, ticketid, numberoftickets, method)
+    End Sub
+    'This Sub Contains The Code That Reads The Data
+    Sub readdata(ByRef customerid, ByRef ticketid, ByRef numberoftickets, ByRef method)
+        'Declaring Local Variables
+        Dim newsentence, filename As String
+        Dim counter As Integer
+        Dim Temp(3) As String
+        'Telling the computer where to find the data
+        filename = "E:\Higher CS\course work\Choral Shield Data File.csv"
+        'the computer reading the lines of the csv file and putting them into arrays
+        Dim objtextfile As New System.IO.StreamReader(filename)
+        For counter = 0 To 299
+            newsentence = objtextfile.ReadLine()
+            'Splits Data At Delimeter
+            Temp = newsentence.Split(","c)
+            customerid(counter) = Temp(0)
+            ticketid(counter) = Temp(1)
+            numberoftickets(counter) = Temp(2)
+            method(counter) = Temp(3)
+        Next
+        'Closing and Disposing the file
+        objtextfile.Close()
+        objtextfile.Dispose()
+    End Sub
+    'This Sub Contains The Code That Calculates The Data
+    Sub calculations(ByRef customerid, ByRef ticketid, ByRef numberoftickets, ByRef method)
+        'Declaring Local Variables
+        Dim counter, popular, total As Integer
+        Dim msg As String
+        'Making the counter so that if people buy the tickets online then it will add one to the counter a.k.a popular
+        For counter = 0 To 299
+            If method(counter) = "O" Then
+                popular = popular + 1
+            End If
+        Next
+        'If the counter popular is over 250 sales then thats more than half the tickets so the best sale method would be Online else School
+        If popular > 250 Then
+            msg = "The overall favorite method of purchase was Online"
+        Else
+            msg = "The overall favorite method of purchase was School"
+        End If
+        'Clearing the total
+        total = 0
+        'Pricing the nights Friday = £10 Rest = £5
+        For counter = 0 To 299
+            If ticketid(counter).substring(0, 1) = "F" Then
+                total = total + numberoftickets(counter) * 10
+            Else
+                total = total + numberoftickets(counter) * 5
+            End If
+        Next
+        LbResults.Items.Add("Essell Academy Choral Shield " & DateTime.Now.Year)
+        LbResults.Items.Add(msg)
+        LbResults.Items.Add("The amount of money raised is " & "£" & total)
+    End Sub
+    'This Sub Contains The Code That Writes The Data
+    Sub writedata(ByRef customerid, ByRef ticketid, ByRef numberoftickets, ByRef method)
+        'Declaring Local Variables
+        Dim counter As Integer
+        Dim newsentence, filename As String
+        'setting filename to the destination of the file
+        filename = "E:\Higher CS\course work\Test1 (2).csv"
+        Dim objtextfile As New System.IO.StreamWriter(filename)
+        For counter = 0 To 299
+            If ticketid(counter).Substring(0, 1) = "F" Then
+                newsentence = customerid(counter) & "," & ticketid(counter) & "," & numberoftickets(counter) & "," & method(counter)
+                objtextfile.WriteLine(newsentence)
+            End If
+        Next
+        'Closing The Open File
+        objtextfile.Close()
+    End Sub
+    'This Button Clears The Text From The Listbox
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_clear.Click
+        LbResults.Items.Clear()
+    End Sub
+    'This Button Exits From The Program
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_exit.Click
+        Close()
+    End Sub
+
+End Class
